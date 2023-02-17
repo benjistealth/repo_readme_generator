6f57268d9c0f3e42e8d149708b416d05a1a8bdfb
Ben <stealthton@gmail.com>
2023-02-17 15:04:28 +0000

    added user prompt and dependancies

--- assets/utils/generateMarkdown.js
+++ assets/utils/generateMarkdown.js
@@ -1,6 +1,63 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(readmeDetails) {
+  return `# ${readmeDetails.title}
+
+A node.js app that generates a readme.md file from user input at the command line.
+
+## Description
+
+- ${readmeDetails.description}
+
+
+## ${readmeDetails.screenshot1}
+
+    
+  <img alt="Screenshot_1" src="assets\images\screenshot2.png">
+
+
+## ${readmeDetails.screenshot2}
+
+
+  <img alt="Screenshot_2" src="assets\images\screenshot1.png">
+
+
+## Installation
+
+NA - Website hosted on Github Pages
+
+https://benjistealth.github.io/benji_Console-Finances
+
+## Usage
+
+- Visit the page on Github pages, press F12 to open the console and see the financial info
+
+https://benjistealth.github.io/benji_Console-Finances
+
+## Credits
+
+Some credit to Mozilla Developer Network and Stack Overflow for some help with Javascript functions
+
+## License
+
+MIT License - Please see a copy of this in the root of the repository
+
+
+## Badges
+
+NA
+
+## Features
+
+HTML, CSS, Javascript
+
+## How to Contribute
+
+NA
+
+## Tests
+
+Manual - Numerical accuracy checked with MS Excel
+
 
 `;
 }
--- index.js
+++ index.js
@@ -1,15 +1,62 @@
 const fs = require("fs");
+const util = require('util');
 const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-// array of questions for user
-const questions = [
+const writeToFile = util.promisify(fs.writeFile);
 
-];
+// questions for user
+const promptUser = () =>
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'projectTitle',
+            message: 'What is the project name?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Describe the project in a  couple of sentences.',
+        },
+        {
+            type: 'input',
+            name: 'screenshot1',
+            message: 'Describe screenshot1',
+        },
+        {
+            type: 'input',
+            name: 'screenshot1',
+            message: 'Describe screenshot1',
+        },
+        {
+            message: 'Select any of the following badges that you want to display: ',
+            name: 'badges',
+            type: 'checkbox',
+            choices: ['Swimmers Badge', 'Medal of Honour', 'JS', 'HTML', 'CSS', 'Bootstrap', 'Node.js', 'Express.js',],
+        },
+        {
+            type: 'input',
+            name: 'githubPages',
+            message: 'Enter your Github Pages URL',
+        },
+        {
+            type: 'input',
+            name: 'testing',
+            message: 'What testing was carried out on the project?',
+        },
+    ]);
+
+promptUser()
+  // promises to handle async calls
+  .then((readmeDetails) => writeToFile('readme.md', generateMarkdown(readmeDetails)))
+  .then(() => console.log('Successfully created Readme.md with your information.'))
+  .catch((err) => console.error(err));
+
+
 
 // function to write README file
-function writeToFile(fileName, data) {
+function writeToFile(fileName, readmeDetails) {
 }
 
 // function to initialize program
